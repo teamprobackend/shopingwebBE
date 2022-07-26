f65cd41936456d9ec0adfd7a799292f50273a687
teamprobackend <108886099+teamprobackend@users.noreply.github.com>
2022-07-26 04:26:57 -0400

    Delete webcrawler directory

--- webcrawler/insertRoles.js
+++
@@ -1,8 +0,0 @@
-const db = require('../models/index');
-let roles = ["user","staff","admin"];
-async function insertRoles(){
-  for(let role of roles){
-    await db.Roles.create({name: role});
-  }
-}
-insertRoles();
