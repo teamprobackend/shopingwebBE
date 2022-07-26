244edc13cb5c6b8a9bcb590709a5aeabbba30293
teamprobackend <108886099+teamprobackend@users.noreply.github.com>
2022-07-26 04:28:15 -0400

    Delete demo directory

--- demo/shopingwebBE-main/controller/productController.js
+++
@@ -1,4 +0,0 @@
-const db = require('./../models/index')
-const factory = require('./handlerFactory')
-
-exports.getAllProduct = factory.getAll(db.Product)
--- demo/shopingwebBE-main/route/productRouter.js
+++
@@ -1,4 +0,0 @@
-const router = require('express').Router()
-const producController = require('./../controller/productController')
-router.route('/').get(producController.getAllProduct)
-module.exports = router
